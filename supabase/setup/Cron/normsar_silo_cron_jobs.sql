47a7e5048856b7c3a9aea7c549f23378945777dc
sengtha <sengtha@gmail.com>
2026-04-29 12:06:35 +0700

    Add files via upload

---
+++ supabase/setup/Cron/normsar_silo_cron_jobs.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict m9DB5iPBaA2cd4Bc74opWAhJ9RYvhKqfEKzuMNbhyAnqDfuqmoki5HQH5VJmbl4
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 18.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict m9DB5iPBaA2cd4Bc74opWAhJ9RYvhKqfEKzuMNbhyAnqDfuqmoki5HQH5VJmbl4
+
